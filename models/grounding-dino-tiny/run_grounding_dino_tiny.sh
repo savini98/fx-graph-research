3736c00f8146c1f96b3247e6a7620b948f4db648
savini98 <savikashmira@gmail.com>
2026-03-18 09:10:50 -0400

    model

---
+++ models/grounding-dino-tiny/run_grounding_dino_tiny.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# set -x
+ORIGINAL="original"
+FIXED="fixed"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+mkdir -p traces
+mkdir -p original_model_files
+echo "Activating conda environment..."
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate "${ORIGINAL}_env"
+echo "Activated conda environment"
+
+# Running grounding-dino-tiny model original
+echo "Running grounding-dino-tiny original model..."
+python grounding_dino_tiny_script.py \
+    --type $ORIGINAL \
+    --runs 30 \
+    --batch_size 8 2>&1 | tee traces/grounding_dino_tiny_original_model_output_$(date +"%Y%m%d_%H%M%S").log
+
+echo "Completed running grounding-dino-tiny original model."
+
+echo "Running grounding-dino-tiny fixed model..."
+
+echo "Activating conda environment..."
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate "${FIXED}_env"
+echo "Activated conda environment"
+
+# Running grounding-dino-tiny model fixed
+echo "Running grounding-dino-tiny fixed model..."
+python grounding_dino_tiny_script.py \
+    --type $FIXED \
+    --runs 30 \
+    --batch_size 8 2>&1 | tee traces/grounding_dino_tiny_fixed_model_output_$(date +"%Y%m%d_%H%M%S").log
