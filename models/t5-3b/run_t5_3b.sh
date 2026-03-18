570e173466d9d218357baf617f8c109d9f8dfd2c
savini98 <savikashmira@gmail.com>
2026-03-18 08:59:20 -0400

    new model

---
+++ models/t5-3b/run_t5_3b.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# set -x
+ORIGINAL="original"
+FIXED="fixed"
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+mkdir -p traces
+mkdir -p original_model_files
+echo "Activating conda environment..."
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate "${ORIGINAL}_env"
+echo "Activated conda environment"
+
+# Running t5-3b model original
+echo "Running t5-3b original model..."
+python t5_3b_script.py \
+    --type $ORIGINAL \
+    --runs 30 \
+    --batch_size 50 2>&1 | tee traces/t5_3b_original_model_output_$(date +"%Y%m%d_%H%M%S").log
+
+echo "Completed running t5-3b original model."
+
+echo "Running t5-3b fixed model..."
+
+echo "Activating conda environment..."
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate "${FIXED}_env"
+echo "Activated conda environment"
+
+# Running t5-3b model fixed
+echo "Running t5-3b fixed model..."
+python t5_3b_script.py \
+    --type $FIXED \
+    --runs 30 \
+    --batch_size 50 2>&1 | tee traces/t5_3b_fixed_model_output_$(date +"%Y%m%d_%H%M%S").log
